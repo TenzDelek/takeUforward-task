bf4dd23e2459d2db8f50c9bc990665a93655900a
TenzDelek <tibetdelek@gmail.com>
2024-08-11 23:59:27 +0530

    build error fix

--- src/app/api/banner/[id]/route.ts
+++ src/app/api/banner/[id]/route.ts
@@ -1,23 +1,7 @@
 import prisma from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function DELETE({ params }: { params: { id: string } }) {
-  try {
-    const id = params.id;
-    await prisma.bannerSettings.delete({
-      where: { id: parseInt(id) },
-    });
-    return NextResponse.json({ message: "Banner deleted successfully" });
-  } catch (error) {
-    console.error("Database error:", error);
-    return NextResponse.json(
-      { error: "Internal Server Error" },
-      { status: 500 }
-    );
-  } finally {
-    await prisma.$disconnect();
-  }
-}
+
 
 export async function PATCH(
   request: NextRequest,
